46fb588db059122b50570cf4883d5e4eb7c55e31
manumanchi <manoj.manchikanti@gmail.com>
2016-04-21 01:41:49 -0400

    added some shell script for reading

---
+++ print.sh
@@ -0,0 +1,19 @@
+#!/bin/
+#echo "Hello $USER"
+#echo "Today is "; date
+#echo "Number of user login:" ; who | wc -l
+#echo "Calandar"
+#cal
+#echo "Present working directory:" $PWD
+#xn=Rani
+#echo $xn
+#expr 6 + 3
+#x=20
+#y=5
+#z=$(($x/$y))
+#echo "z:$z"
+#echo -e "\033[32m" 
+#to change foreground colour 30 to 37 & to change backcground colour 40 to 47
+expr 5 + 10
+echo $?
+
